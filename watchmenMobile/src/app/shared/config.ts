3555ae59b36cb3e54754b9321a0e5bb24a583f6d
juan.marriaga <jesteban2@gmail.com>
2020-02-10 19:38:57 -0500

    user schema change

--- watchmenMobile/src/app/shared/config.ts
+++ watchmenMobile/src/app/shared/config.ts
@@ -1,8 +1,8 @@
 import * as app from 'application'
 
 //const API = app.android ? 'https://10.0.2.2:8443' : 'https://localhost:8443'////
-const API = 'http://192.168.1.128:3000'
-//const API = 'http://ec2-18-234-227-124.compute-1.amazonaws.com:3000'
+//const API = 'http://192.168.1.128:3000'
+const API = 'http://ec2-18-234-227-124.compute-1.amazonaws.com:3000'
 
 export class Config {
   static loginApiUrl = API+"/user/login";
--- watchmenServer/src/com/watchmen/services/user/model/User.js
+++ watchmenServer/src/com/watchmen/services/user/model/User.js
@@ -24,6 +24,16 @@ const userSchema = mongoose.Schema({
         required: true,
         trim: true
     },
+    address: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true
+    },
     password: {
         type: String,
         required: true,
